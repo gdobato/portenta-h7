7bc0dd5e4273cdad86de2b792a9ef10f25c2ef56
Gabriel Dobato <dobatog@gmail.com>
2023-04-04 23:02:13 +0200

    Move bsp to lib

--- examples/led_blinky.rs
+++ examples/led_blinky.rs
@@ -4,21 +4,14 @@
 #![no_main]
 
 use core::cell::RefCell;
-use cortex_m_rt::entry;
-use hal::{gpio::*, pac, prelude::*, rcc};
 use non_preemptive_scheduler::{resources::UnShared, EventMask, Scheduler, Task};
 use non_preemptive_scheduler_macros as scheduler;
-#[allow(unused_imports)]
-use rtt_target::{rprintln as log, rtt_init_print as log_init};
-use stm32h7xx_hal as hal;
+use portenta_rs::{BlueUserLed, GreenUserLed, RedUserLed, UserLeds};
 
 // Static and interior mutable entities
-static RED_LED: UnShared<RefCell<Option<PK5<Output<PushPull>>>>> =
-    UnShared::new(RefCell::new(None));
-static GREEN_LED: UnShared<RefCell<Option<PK6<Output<PushPull>>>>> =
-    UnShared::new(RefCell::new(None));
-static BLUE_LED: UnShared<RefCell<Option<PK7<Output<PushPull>>>>> =
-    UnShared::new(RefCell::new(None));
+static RED_LED: UnShared<RefCell<Option<RedUserLed>>> = UnShared::new(RefCell::new(None));
+static GREEN_LED: UnShared<RefCell<Option<GreenUserLed>>> = UnShared::new(RefCell::new(None));
+static BLUE_LED: UnShared<RefCell<Option<BlueUserLed>>> = UnShared::new(RefCell::new(None));
 
 // Create scheduler
 #[scheduler::new(task_count = 3, core_freq = 480_000_000)]
@@ -39,53 +32,21 @@ fn green_led_blinky(_: EventMask) {
 
 fn blue_led_blinky(_: EventMask) {
     if let Some(blue_led) = BLUE_LED.borrow().borrow_mut().as_mut() {
+        portenta_rs::log!("blinky");
         blue_led.toggle();
     }
 }
 
-fn bsp_init() {
-    let sysclk = portenta_rs::sys::Clk::take().unwrap();
-    let dp = pac::Peripherals::take().unwrap();
-
-    sysclk.reset().enable_ext_clock();
-
-    let pwr = dp.PWR.constrain();
-    let pwrcfg = pwr.vos0(&dp.SYSCFG).freeze();
-
-    let ccdr = dp
-        .RCC
-        .constrain()
-        .use_hse(27.MHz())
-        .bypass_hse()
-        .sys_ck(480.MHz())
-        .hclk(240.MHz())
-        .pll1_strategy(rcc::PllConfigStrategy::Iterative)
-        .pll1_q_ck(240.MHz())
-        .pll2_strategy(rcc::PllConfigStrategy::Iterative)
-        .pll2_p_ck(100.MHz())
-        .pll3_strategy(rcc::PllConfigStrategy::Iterative)
-        .pll3_p_ck(100.MHz())
-        .pll3_r_ck(100.MHz())
-        .freeze(pwrcfg, &dp.SYSCFG);
+#[portenta_rs::entry]
+fn main() -> ! {
+    let board = portenta_rs::Board::init().unwrap();
 
     // User LEDs
-    let gpiok = dp.GPIOK.split(ccdr.peripheral.GPIOK);
-    RED_LED.borrow().replace(Some(
-        gpiok.pk5.into_push_pull_output_in_state(PinState::High),
-    ));
-    GREEN_LED.borrow().replace(Some(
-        gpiok.pk6.into_push_pull_output_in_state(PinState::High),
-    ));
-    BLUE_LED.borrow().replace(Some(
-        gpiok.pk7.into_push_pull_output_in_state(PinState::High),
-    ));
-}
+    let UserLeds { red, green, blue } = board.user_leds;
 
-#[entry]
-fn main() -> ! {
-    log_init!();
-
-    bsp_init();
+    RED_LED.borrow().replace(Some(red));
+    GREEN_LED.borrow().replace(Some(green));
+    BLUE_LED.borrow().replace(Some(blue));
 
     // Add tasks
     scheduler::add_task!(
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,98 @@
 #![no_std]
+
 pub mod pmic;
 pub mod sys;
+
+pub use cortex_m_rt::entry;
+#[allow(unused_imports)]
+pub use rtt_target::rprintln as log;
+
+use hal::{
+    gpio::{self, *},
+    pac,
+    prelude::*,
+    rcc,
+};
+use rtt_target::rtt_init_print as log_init;
+use stm32h7xx_hal as hal;
+
+pub type RedUserLed = gpio::gpiok::PK5<Output<PushPull>>;
+pub type GreenUserLed = gpio::gpiok::PK6<Output<PushPull>>;
+pub type BlueUserLed = gpio::gpiok::PK7<Output<PushPull>>;
+
+pub struct UserLeds {
+    pub red: RedUserLed,
+    pub green: GreenUserLed,
+    pub blue: BlueUserLed,
+}
+
+pub struct Board {
+    pub user_leds: UserLeds,
+}
+
+impl Board {
+    pub fn init() -> Option<Board> {
+        static mut INITIALIZED: bool = false;
+        if unsafe { INITIALIZED } {
+            None
+        } else {
+            log_init!();
+            #[cfg(debug_assertions)]
+            log!("Board init");
+
+            let dp = pac::Peripherals::take().unwrap();
+            let sysclk = sys::Clk::take().unwrap();
+
+            // Reset previous configuration and enable external oscillator
+            sysclk.reset().enable_ext_clock();
+
+            // Configure power domains and clock tree
+            let pwr = dp.PWR.constrain();
+            let pwrcfg = pwr.vos0(&dp.SYSCFG).freeze();
+            let ccdr = dp
+                .RCC
+                .constrain()
+                .use_hse(25.MHz())
+                .bypass_hse()
+                .sys_ck(480.MHz())
+                .hclk(240.MHz())
+                .pll1_strategy(rcc::PllConfigStrategy::Iterative)
+                .pll1_q_ck(240.MHz())
+                .pll2_strategy(rcc::PllConfigStrategy::Iterative)
+                .pll2_p_ck(100.MHz())
+                .pll3_strategy(rcc::PllConfigStrategy::Iterative)
+                .pll3_p_ck(100.MHz())
+                .pll3_r_ck(100.MHz())
+                .freeze(pwrcfg, &dp.SYSCFG);
+
+            #[cfg(debug_assertions)]
+            unsafe {
+                let rcc_blck = &(*pac::RCC::PTR);
+                log!(
+                    "Sysclk source: {:?}, PLLs selection: {:?}",
+                    rcc_blck.cfgr.read().sws().variant(),
+                    rcc_blck.pllckselr.read().pllsrc().variant()
+                );
+            }
+
+            // Configure User LEDs
+            let gpiok = dp.GPIOK.split(ccdr.peripheral.GPIOK);
+            let (red_led, green_led, blue_led) = (
+                gpiok.pk5.into_push_pull_output_in_state(PinState::High),
+                gpiok.pk6.into_push_pull_output_in_state(PinState::High),
+                gpiok.pk7.into_push_pull_output_in_state(PinState::High),
+            );
+
+            unsafe {
+                INITIALIZED = true;
+            }
+            Some(Board {
+                user_leds: UserLeds {
+                    red: red_led,
+                    green: green_led,
+                    blue: blue_led,
+                },
+            })
+        }
+    }
+}
