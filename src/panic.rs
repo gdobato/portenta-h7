9eb8941939b152f0c3aa86e8a4aed66cf41535ed
Gabriel Dobato <dobatog@gmail.com>
2023-04-05 05:12:12 +0200

    Add panic handler. Clean up

---
+++ src/panic.rs
@@ -0,0 +1,13 @@
+//!  Panic
+
+use core::panic::PanicInfo;
+#[cfg(debug_assertions)]
+use rtt_target::rprintln as log;
+
+#[panic_handler]
+#[allow(unused_variables)]
+fn panic(info: &PanicInfo) -> ! {
+    #[cfg(debug_assertions)]
+    log!("{}", info);
+    loop {}
+}
