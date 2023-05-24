6771ec283a667553b6c994775b6cb997887de4bd
Gabriel Dobato <dobatog@gmail.com>
2023-05-25 01:43:16 +0200

    Replace basic scheduler by RTIC

---
+++ examples/blinky.rs
@@ -0,0 +1,85 @@
+//! Example Blinky
+//!
+//! Toggles the 3 user LEDs with a different frequency
+//!
+
+#![no_std]
+#![no_main]
+
+#[allow(unused_imports)]
+use portenta_h7::{log, log_init, user_led};
+use rtic::app;
+use systick_monotonic::{fugit::MillisDurationU64, Systick};
+
+#[app(device = portenta_h7::hal::pac, peripherals = false, dispatchers = [SPI1])]
+mod app {
+    use super::*;
+
+    #[shared]
+    struct Shared {}
+
+    #[local]
+    struct Local {
+        led_red: user_led::Red,
+        led_green: user_led::Green,
+        led_blue: user_led::Blue,
+    }
+
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1_000>;
+
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        log_init!();
+
+        let mono = Systick::new(cx.core.SYST, portenta_h7::CORE_FREQUENCY.raw());
+
+        // Get boatd resources
+        let portenta_h7::Board {
+            led_red,
+            led_green,
+            led_blue,
+            ..
+        } = portenta_h7::Board::take();
+
+        #[cfg(debug_assertions)]
+        log!("spawning tasks");
+        blink_led_red::spawn_after(MillisDurationU64::from_ticks(0)).unwrap();
+        blink_led_green::spawn_after(MillisDurationU64::from_ticks(0)).unwrap();
+        blink_led_blue::spawn_after(MillisDurationU64::from_ticks(0)).unwrap();
+
+        (
+            Shared {},
+            Local {
+                led_red,
+                led_green,
+                led_blue,
+            },
+            init::Monotonics(mono),
+        )
+    }
+
+    #[task(local = [led_red])]
+    fn blink_led_red(cx: blink_led_red::Context) {
+        #[cfg(debug_assertions)]
+        log!("toggling {:?}", cx.local.led_red);
+        cx.local.led_red.toggle();
+        blink_led_red::spawn_after(MillisDurationU64::from_ticks(500)).unwrap();
+    }
+
+    #[task(local = [led_green])]
+    fn blink_led_green(cx: blink_led_green::Context) {
+        #[cfg(debug_assertions)]
+        log!("toggling {:?}", cx.local.led_green);
+        cx.local.led_green.toggle();
+        blink_led_green::spawn_after(MillisDurationU64::from_ticks(1_000)).unwrap();
+    }
+
+    #[task(local = [led_blue])]
+    fn blink_led_blue(cx: blink_led_blue::Context) {
+        #[cfg(debug_assertions)]
+        log!("toggling {:?}", cx.local.led_blue);
+        cx.local.led_blue.toggle();
+        blink_led_blue::spawn_after(MillisDurationU64::from_ticks(2_000)).unwrap();
+    }
+}
--- examples/usb_echo.rs
+++ examples/usb_echo.rs
@@ -6,125 +6,91 @@
 #![no_std]
 #![no_main]
 
-#[allow(unused_imports)]
-use core::{cell::RefCell, str::from_utf8};
 use cortex_m::singleton;
-use non_preemptive_scheduler as scheduler;
 use portenta_h7::{
-    entry,
-    interrupt::{interrupt, InterruptEnabler},
     log, log_init,
     user_led::{self, UserLed},
     UsbBus, USB,
 };
-use scheduler::{resources::UnShared, EventMask, Scheduler, Task};
-use usb_device::{class_prelude::*, prelude::*};
+use rtic::app;
+use systick_monotonic::Systick;
+use usb_device::prelude::*;
 use usbd_serial::CdcAcmClass;
 
-// Events
-const EVENT_USB_ENUMERATION_COMPLETED: EventMask = 0x00000001;
-const EVENT_USB_ENUMERATION_LOST: EventMask = 0x00000002;
-
-// Static interior mutable entities
-static LED_BLUE: UnShared<RefCell<Option<user_led::Blue>>> = UnShared::new(RefCell::new(None));
-static USB_SERIAL_PORT: UnShared<RefCell<Option<CdcAcmClass<UsbBus<USB>>>>> =
-    UnShared::new(RefCell::new(None));
-static USB_DEV: UnShared<RefCell<Option<UsbDevice<UsbBus<USB>>>>> =
-    UnShared::new(RefCell::new(None));
-// Static mutable entities
 const USB_BUS_BUFFER_SIZE: usize = 1024;
 static mut USB_BUS_BUFFER: [u32; USB_BUS_BUFFER_SIZE] = [0u32; USB_BUS_BUFFER_SIZE];
 const USB_HS_MAX_PACKET_SIZE: usize = 512;
 static mut USB_APP_BUFFER: [u8; USB_HS_MAX_PACKET_SIZE] = [0u8; USB_HS_MAX_PACKET_SIZE];
 
-// Create scheduler
-#[scheduler::new(task_count = 2, core_freq = 480_000_000)]
-struct NonPreemptiveScheduler;
+#[app(device = portenta_h7::hal::pac, peripherals = false)]
+mod app {
+    use super::*;
 
-// Functions which are bound to task runnables
-fn led_process(event_mask: EventMask) {
-    match event_mask & (EVENT_USB_ENUMERATION_COMPLETED | EVENT_USB_ENUMERATION_LOST) {
-        EVENT_USB_ENUMERATION_COMPLETED => {
-            if let Some(led_blue) = LED_BLUE.borrow().borrow_mut().as_mut() {
-                log!("Enumeration completed");
-                led_blue.set_on();
-            }
-        }
+    #[shared]
+    struct Shared {}
 
-        EVENT_USB_ENUMERATION_LOST => {
-            if let Some(led_blue) = LED_BLUE.borrow().borrow_mut().as_mut() {
-                log!("Enumeration lost");
-                led_blue.set_off();
-            }
-        }
-        _ => (),
+    #[local]
+    struct Local {
+        led_blue: user_led::Blue,
+        usb_dev: UsbDevice<'static, UsbBus<USB>>,
+        usb_serial_port: CdcAcmClass<'static, UsbBus<USB>>,
     }
-}
 
-#[entry]
-fn main() -> ! {
-    log_init!();
+    #[monotonic(binds = SysTick, default = true)]
+    type MonoTimer = Systick<1_000>;
 
-    let portenta_h7::Board {
-        mut cp,
-        led_blue,
-        usb,
-        ..
-    } = portenta_h7::Board::take();
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        log_init!();
 
-    // Configure Usb CDC class
-    usb.set_interrupt_priority(&mut cp, 3);
-    usb.enable_interrupt();
-    let usb_bus: &'static UsbBusAllocator<UsbBus<USB>> = singleton!(
-        USB_BUS: UsbBusAllocator<UsbBus<USB>> = UsbBus::new(usb, unsafe { &mut USB_BUS_BUFFER })
-    )
-    .unwrap();
-    USB_SERIAL_PORT
-        .borrow()
-        .replace(Some(usbd_serial::CdcAcmClass::new(
-            usb_bus,
-            USB_HS_MAX_PACKET_SIZE as u16,
-        )));
-    USB_DEV.borrow().replace(Some(
-        UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x1234, 0xABCD))
+        let mono = Systick::new(cx.core.SYST, portenta_h7::CORE_FREQUENCY.raw());
+
+        // Get board resources
+        let portenta_h7::Board { led_blue, usb, .. } = portenta_h7::Board::take();
+
+        // Init USB stack
+        let usb_bus = singleton!(
+            : usb_device::class_prelude::UsbBusAllocator<UsbBus<USB>> =
+                UsbBus::new(usb, unsafe { &mut USB_BUS_BUFFER })
+        )
+        .unwrap();
+        let usb_serial_port = usbd_serial::CdcAcmClass::new(usb_bus, USB_HS_MAX_PACKET_SIZE as u16);
+        let usb_dev = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x1234, 0xABCD))
             .manufacturer("example")
             .product("usb-echo")
             .serial_number("0123456789ABCDEF")
             .device_class(usbd_serial::USB_CLASS_CDC)
             .max_packet_size_0(64)
             .max_power(100)
-            .build(),
-    ));
+            .build();
 
-    // Initialize LED to signal enumeration state
-    LED_BLUE.borrow().replace(Some(led_blue));
-
-    // Add tasks to scheduler and launch it
-    scheduler::add_task!("led", None, Some(led_process), None, None);
-    scheduler::launch!();
-
-    loop {
-        panic!("Not expected execution");
+        (
+            Shared {},
+            Local {
+                led_blue,
+                usb_dev,
+                usb_serial_port,
+            },
+            init::Monotonics(mono),
+        )
     }
-}
 
-#[interrupt]
-fn OTG_HS() {
-    if let (Some(usb_dev), Some(usb_serial_port)) = (
-        USB_DEV.borrow().borrow_mut().as_mut(),
-        USB_SERIAL_PORT.borrow().borrow_mut().as_mut(),
-    ) {
-        // Previous state before polling
+    #[task(binds = OTG_HS, local = [led_blue, usb_dev, usb_serial_port])]
+    fn usb_process(cx: usb_process::Context) {
+        let (usb_dev, usb_serial_port) = (cx.local.usb_dev, cx.local.usb_serial_port);
         let previous_state = usb_dev.state();
+        // Trigger internal state machine. It should be call either from ISR on USB event,
+        // or every 10 ms from normal execution context
         if usb_dev.poll(&mut [usb_serial_port]) {
-            // Read from reception fifo.
+            // Read from reception fifo
             match usb_serial_port.read_packet(unsafe { &mut USB_APP_BUFFER[..] }) {
                 Ok(cnt) if cnt > 0 => {
                     #[cfg(debug_assertions)]
                     log!(
                         "Received {} bytes: {}",
                         cnt,
-                        from_utf8(unsafe { &USB_APP_BUFFER[..cnt] }).unwrap_or("not valid")
+                        core::str::from_utf8(unsafe { &USB_APP_BUFFER[..cnt] })
+                            .unwrap_or("not valid")
                     );
                     // Send back received data
                     match usb_serial_port.write_packet(unsafe { &USB_APP_BUFFER[..cnt] }) {
@@ -138,17 +104,19 @@ fn OTG_HS() {
             }
         }
 
-        // Current state after polling
+        // Signal enumeration status
         match usb_dev.state() {
             // Transition to enumeration
             UsbDeviceState::Configured if previous_state == UsbDeviceState::Addressed => {
-                scheduler::set_task_event!("led", EVENT_USB_ENUMERATION_COMPLETED);
+                log!("Enumeration completed");
+                cx.local.led_blue.set_on();
             }
             // Already enumerated
             UsbDeviceState::Configured => {}
             // Enumeration lost
             _ if previous_state == UsbDeviceState::Configured => {
-                scheduler::set_task_event!("led", EVENT_USB_ENUMERATION_LOST);
+                log!("Enumeration lost");
+                cx.local.led_blue.set_off();
             }
             _ => (),
         }
